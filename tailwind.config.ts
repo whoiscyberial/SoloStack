b488266efed94b22229a989d8bce89860649bfeb
whoiscyberial <whoiscyberial@gmail.com>
2023-10-09 18:22:43 +0500

    initial

---
+++ tailwind.config.ts
@@ -0,0 +1,10 @@
+import { type Config } from "tailwindcss";
+
+export default {
+  darkMode: "class",
+  content: ["./src/**/*.{js,ts,jsx,tsx}"],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+} satisfies Config;
