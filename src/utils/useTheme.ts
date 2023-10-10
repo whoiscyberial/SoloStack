8323f9f1268640c4a23c5beb57b56d9db7bf50c3
whoiscyberial <whoiscyberial@gmail.com>
2023-10-10 21:11:31 +0500

    added useTheme hook

---
+++ src/utils/useTheme.ts
@@ -0,0 +1,16 @@
+import React, { useEffect } from "react";
+
+export const useTheme = () => {
+  useEffect(() => {
+    if (
+      localStorage.theme === "dark" ||
+      (!("theme" in localStorage) &&
+        window.matchMedia("(prefers-color-scheme: dark)").matches)
+    ) {
+      document.documentElement.classList.add("dark");
+    } else {
+      document.documentElement.classList.remove("dark");
+    }
+    localStorage.theme = "dark";
+  }, []);
+};
