9f8a79cd60353a968aa585c370d56e37d509d384
whoiscyberial <whoiscyberial@gmail.com>
2023-10-12 18:53:48 +0500

    delete verification icons

--- src/components/ToolPage.tsx
+++ src/components/ToolPage.tsx
@@ -51,10 +51,7 @@ const ToolPage = ({ subcategorySlug, toolId }: ToolPage) => {
           />
         </svg>
       </Link>
-      <div className="flex items-center  gap-4">
-        <h2>{tool.data.title}</h2>
-        <span>{tool.data.verified ? <Verified /> : <NotVerified />}</span>
-      </div>
+      <h2>{tool.data.title}</h2>
       <span className="mt-1">{tool.data.description}</span>
       {tool.data.text && (
         <div className="mt-10">
@@ -66,44 +63,6 @@ const ToolPage = ({ subcategorySlug, toolId }: ToolPage) => {
         Check out {tool.data.title}
       </Button>
     </motion.div>
-  );
-};
-
-const Verified = () => {
-  return (
-    <svg
-      xmlns="http://www.w3.org/2000/svg"
-      fill="none"
-      viewBox="0 0 24 24"
-      strokeWidth={1.5}
-      stroke="currentColor"
-      className="-mx-3 -my-6 h-7 w-7 transition-all dark:text-neutral-500 dark:hover:text-emerald-500"
-    >
-      <path
-        strokeLinecap="round"
-        strokeLinejoin="round"
-        d="M9 12.75L11.25 15 15 9.75M21 12c0 1.268-.63 2.39-1.593 3.068a3.745 3.745 0 01-1.043 3.296 3.745 3.745 0 01-3.296 1.043A3.745 3.745 0 0112 21c-1.268 0-2.39-.63-3.068-1.593a3.746 3.746 0 01-3.296-1.043 3.745 3.745 0 01-1.043-3.296A3.745 3.745 0 013 12c0-1.268.63-2.39 1.593-3.068a3.745 3.745 0 011.043-3.296 3.746 3.746 0 013.296-1.043A3.746 3.746 0 0112 3c1.268 0 2.39.63 3.068 1.593a3.746 3.746 0 013.296 1.043 3.746 3.746 0 011.043 3.296A3.745 3.745 0 0121 12z"
-      />
-    </svg>
-  );
-};
-
-const NotVerified = () => {
-  return (
-    <svg
-      xmlns="http://www.w3.org/2000/svg"
-      fill="none"
-      viewBox="0 0 24 24"
-      strokeWidth={1.5}
-      stroke="currentColor"
-      className="-mx-3 -my-6 h-7 w-7  transition-all dark:text-neutral-500 dark:hover:text-red-600"
-    >
-      <path
-        strokeLinecap="round"
-        strokeLinejoin="round"
-        d="M9.75 9.75l4.5 4.5m0-4.5l-4.5 4.5M21 12a9 9 0 11-18 0 9 9 0 0118 0z"
-      />
-    </svg>
   );
 };
 
