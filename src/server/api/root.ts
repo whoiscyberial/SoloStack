c3d8fa3bf078b1af537aa2172a2aeb3841b0bc9c
whoiscyberial <whoiscyberial@gmail.com>
2023-11-04 01:27:49 +0500

    delete post router

--- src/server/api/root.ts
+++ src/server/api/root.ts
@@ -1,5 +1,4 @@
 import { toolRouter } from "@/server/api/routers/toolRouter";
-import { postRouter } from "@/server/api/routers/postRouter";
 import { categoryRouter } from "@/server/api/routers/categoryRouter";
 import { subcategoryRouter } from "@/server/api/routers/subcategoryRouter";
 import { userRouter } from "./routers/userRouter";
@@ -9,7 +8,6 @@ import { createTRPCRouter } from "@/server/api/trpc";
 // primary router
 export const appRouter = createTRPCRouter({
   tool: toolRouter,
-  post: postRouter,
   category: categoryRouter,
   subcategory: subcategoryRouter,
   user: userRouter,
